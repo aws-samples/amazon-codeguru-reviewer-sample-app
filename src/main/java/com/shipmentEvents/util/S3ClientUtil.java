0bdebdf195c1e2867db1ff33fb5b85a84b610d31
Triet <triet05@gmail.com>
2020-08-02 09:49:49 -0400

    Update S3ClientUtil.java

--- src/main/java/com/shipmentEvents/util/S3ClientUtil.java
+++ src/main/java/com/shipmentEvents/util/S3ClientUtil.java
@@ -7,6 +7,9 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 public class S3ClientUtil {
 
     public static AmazonS3 getS3Client() {
+        int i = 0;
+        for (int j=0; j < 10; j++)
+            i = i + j;
         return AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
     }
     
