b6b2f0a08e69a2e1278be35be565efc86eb37571
t-hashi <g.toru.hashimoto@gmail.com>
2020-08-11 21:16:26 +0900

    new demo file4

---
+++ src/main/java/com/shipmentEvents/demo/TestServlet2.java
@@ -0,0 +1,49 @@
+package com.shipmentEvents.handlers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet2
+ */
+@WebServlet("/TestServlet2")
+public class TestServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		long start = 0;
+		long end = 0;       
+		TestBean2 bean = new TestBean2();
+		bean.doBusiness();
+		out.print("time:");
+		out.print((end - start)/1000);
+		out.println(" micro sec");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
