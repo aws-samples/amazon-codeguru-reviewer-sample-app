c2d682a1cbd168a29fe48412bede6a4603767d90
salmanksears <77377480+salmanksears@users.noreply.github.com>
2021-02-16 16:38:57 +0530

    Update test.java

--- test.java
+++ test.java
@@ -10,3 +10,21 @@ public void givenStaticField_whenLotsOfOperations_thenMemoryLeak() throws Interr
     System.gc();
     Thread.sleep(10000); // to allow GC do its job
 }
+
+
+package com.example.memoryleak;
+public class Adder {
+       publiclong addIncremental(long l)
+       {
+              Long sum=0L;
+               sum =sum+l;
+               return sum;
+       }
+       public static void main(String[] args) {
+              Adder adder = new Adder();
+              for(long ;i<1000;i++)
+              {
+                     adder.addIncremental(i);
+              }
+       }
+}
