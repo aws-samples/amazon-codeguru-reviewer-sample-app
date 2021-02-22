03bf310b39b8b36c4787c107f295207010ed11ed
salmanksears <77377480+salmanksears@users.noreply.github.com>
2021-02-22 18:33:38 +0530

    Update test.java

--- test.java
+++ test.java
@@ -28,3 +28,19 @@ public class Adder {
               }
        }
 }
+package com.example.memoryleak;
+public class Adder {
+       publiclong addIncremental(long l)
+       {
+              Long sum=0L;
+               sum =sum+l;
+               return sum;
+       }
+       public static void main(String[] args) {
+              Adder adder = new Adder();
+              for(long ;i<1000;i++)
+              {
+                     adder.addIncremental(i);
+              }
+       }
+}
