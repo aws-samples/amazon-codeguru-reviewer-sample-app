16e93a18eb770a7552f2e12621a59dd165ea6e8e
master047 <master047@msn.com>
2021-04-24 00:32:52 +0900

    Update S3ClientUtil.java

--- src/main/java/com/shipmentEvents/util/S3ClientUtil.java
+++ src/main/java/com/shipmentEvents/util/S3ClientUtil.java
@@ -6,7 +6,7 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 public class S3ClientUtil {
 
-    public static AmazonS3 getS3Client() {
+    public static AmazonS3 getS3Client() {abcde
         return AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
     }
     
