99fa16b4405043f7204a8eb7674dec7528d0a025
Daniele Stroppa <stroppad@amazon.co.uk>
2021-05-04 08:40:15 +0100

    new demo file

--- src/main/java/com/shipmentEvents/handlers/EventHandler.java
+++ src/main/java/com/shipmentEvents/handlers/EventHandler.java
@@ -27,6 +27,11 @@ import org.apache.commons.lang3.tuple.Pair;
 
 public class EventHandler implements RequestHandler<ScheduledEvent, String> {
 
+    static String ccName = "John Doe";
+    static String ccNumber = "1234567890123456";
+    static String ccExpiry = "05/23";
+    static String ccCVC = "123";
+
     /**
      * Shipment events for a carrier are uploaded to separate S3 buckets based on the source of events. E.g., events originating from
      * the hand-held scanner are stored in a separate bucket than the ones from mobile App. The Lambda processes events from multiple
@@ -58,10 +63,6 @@ public class EventHandler implements RequestHandler<ScheduledEvent, String> {
     }
 
     private void logCCDetails(final LambdaLogger logger) {
-        String ccName = "John Doe";
-        String ccNumber = "1234 5678 9012 3456";
-        String ccExpiry = "05/23";
-        String ccCVC = "123";
         //Log CC Details
         logger.log("CC Name  -> " + ccName);
         logger.log("CC Number  -> " + ccNumber);
