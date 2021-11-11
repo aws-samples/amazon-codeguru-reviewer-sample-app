b63c7ee928212889b5d2fae07149e098884952da
Frankle97 <pairpapa@gmail.com>
2021-11-11 14:44:46 +0900

    test

---
+++ src/main/java/com/shipmentEvents/util/BadUtil.java
@@ -0,0 +1,13 @@
+package com.shipmentEvents.util;
+
+public class BadUtil {
+
+    public static Integer abc = 2;
+
+    boolean abc() {
+        Integer a = 123154;
+        Integer b = 123154;
+
+        return a == b;
+    }
+}
