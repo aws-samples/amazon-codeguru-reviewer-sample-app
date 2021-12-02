abcb85e3e44a6fad0820350a230c78b7d1680374
James Kim <jameskim.careers@gmail.com>
2021-12-02 12:09:30 +1100

    More changes

--- src/main/java/com/shipmentEvents/handlers/EventHandler.java
+++ src/main/java/com/shipmentEvents/handlers/EventHandler.java
@@ -52,7 +52,7 @@ public class EventHandler implements RequestHandler<ScheduledEvent, String> {
             return "SUCCESS";
         } catch (final Exception ex) {
             logger.log(String.format("Failed to process shipment Updates in %s due to %s", scheduledEvent.getAccount(), ex.getMessage()));
-            logger.log();
+            tgdsniogudfniogdf
             throw new RuntimeException(ex);
         }
     }
