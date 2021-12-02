0ab10f33b70896335262be39eca44ac834992d5b
James Kim <james.kim@infotrack.com.au>
2021-12-02 11:37:54 +1100

    Test PR

--- src/main/java/com/shipmentEvents/handlers/EventHandler.java
+++ src/main/java/com/shipmentEvents/handlers/EventHandler.java
@@ -52,6 +52,7 @@ public class EventHandler implements RequestHandler<ScheduledEvent, String> {
             return "SUCCESS";
         } catch (final Exception ex) {
             logger.log(String.format("Failed to process shipment Updates in %s due to %s", scheduledEvent.getAccount(), ex.getMessage()));
+            logger.log();
             throw new RuntimeException(ex);
         }
     }
