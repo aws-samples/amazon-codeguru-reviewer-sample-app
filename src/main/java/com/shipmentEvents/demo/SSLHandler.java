87011535af842e78faa1690e5334d6be7761aae3
s00424258 <shihai3@huawei.com>
2022-08-02 16:50:18 +0800

    add second scenario

---
+++ src/main/java/com/shipmentEvents/demo/SSLHandler.java
@@ -0,0 +1,22 @@
+package com.shipmentEvents.handlers;
+
+import java.security.GeneralSecurityException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.NoSuchAlgorithmException;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+public class SSLHandler {
+    private SSLSocketFactory defaultSslSocketFactory(X509TrustManager trustManager)
+            throws NoSuchAlgorithmException, KeyManagementException {
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            // 第三个参数不能为null，需要补充安全随机数
+            sslContext.init(null, new TrustManager[] { trustManager }, null);
+            return sslContext.getSocketFactory();
+    }
+}
