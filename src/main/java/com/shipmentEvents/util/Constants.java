944ab944ea6f0b3edee05ac162342ed7ac845cc9
deeptech <guptadeepak36@gmail.com>
2022-10-12 14:45:49 +0530

    Update Constants.java

--- src/main/java/com/shipmentEvents/util/Constants.java
+++ src/main/java/com/shipmentEvents/util/Constants.java
@@ -6,5 +6,5 @@ import java.util.List;
 public final class Constants {
 
     public static final List<String> BUCKETS_TO_PROCESS = Arrays.asList("shipment-events-from-scanner", "shipment-events-from-mobile-app");
-    public static final String SUMMARY_BUCKET = "shipment-events-summary"; 
+    public static final String SUMMARY_BUCKET = "shipment-events-summary_detail
 }
