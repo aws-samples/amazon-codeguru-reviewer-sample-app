573ff41fe3f7ed861a1b76248ec25f9d47087a59
sivaprasadreddypkdl <sivaprasadreddypkdl@users.noreply.github.com>
2022-11-18 17:00:51 +0530

    Create EvenasdtHandler.java

---
+++ src/main/java/com/shipmentEvents/test/EvenasdtHandler.java
@@ -0,0 +1,10 @@
+class Output {
+    public static void main(String[] args) {
+    	
+        System.out.println("1. println ");
+        System.out.println("2. println ");
+    	
+        System.out.print("1. print ");
+        System.out.print("2. print");
+    }
+}
