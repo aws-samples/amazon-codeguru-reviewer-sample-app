5cd7760bbf966ec96d894e7b87afda0c87360a21
EC2 Default User <ec2-user@ip-172-31-32-230.us-east-2.compute.internal>
2023-04-12 11:03:23 +0000

    committing demo script

---
+++ reviewer-demo.sh
@@ -0,0 +1,12 @@
+ranNum=$[RANDOM%100+1]
+#echo $ranNum
+branchName="feature"$ranNum
+echo "Checking out to branch name: "$branchName
+git checkout -b $branchName
+mkdir src/main/java/com/shipmentEvents/$branchName
+cp src/main/java/com/shipmentEvents/handlers/EventHandler.java src/main/java/com/shipmentEvents/$branchName/
+echo "Created dummy code in $branchName folder"
+git add --all
+git commit -m "cr3 demo"
+git push --set-upstream origin $branchName
+echo "Committed and pushed dummy code to remote $branchName "
