5d5a94c5821ded871e937236b88fd6b500b8ebbb
MohammadTarek <m.tarek.abdelwahab@gmail.com>
2023-10-28 15:06:33 +0200

    new demo file

--- readme.php
+++ readme.php
@@ -1,3 +1,49 @@
 <?php
-echo "Hello, World!";
-?>
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rlst_buildings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('name_e', 100);
+            $table->text('description')->nullable();
+            $table->text('description_e')->nullable();
+            $table->double('land_area')->nullable()->default(0);
+            $table->double('building_area')->nullable()->default(0)->comment("Building area in Square Meter (<=land area)");
+            $table->char('construction_year', 4)->nullable()->default('2018');
+            $table->unsignedInteger('project_id')->nullable()->default(0);
+            $table->unsignedInteger('country_id')->nullable()->default(0);
+            $table->unsignedInteger('city_id')->nullable()->default(0);
+            $table->unsignedInteger('avenue_id')->nullable()->default(0);
+            $table->unsignedInteger('street_id')->nullable()->default(0);
+            $table->double('lng')->nullable()->default(0);
+            $table->double('lat')->nullable()->default(0);
+            $table->text('properties')->nullable();
+            $table->text('attachments')->nullable();
+            $table->string("module")->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rlst_buildings');
+    }
+};
